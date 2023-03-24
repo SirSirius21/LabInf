31e004001aa965a3837c6f3816e5ff383ef5e3c1
paolo.sabatini <7931893-paolo.sabatini@users.noreply.gitlab.com>
2023-03-24 18:13:05 +0100

    Compito N8.cpp : corretti errori battituta e di codifica (gli accenti non sono ben visti dai "vecchi" compilatori

--- Compito N8.cpp
+++ Compito N8.cpp
@@ -1,23 +1,23 @@
-#include <iostream> //Codice sbagliato.
+#include <iostream> 
 #include <string>
 using namespace std;
-char cifra_più_significativa (int n){
-	returtn to_string(n)[0];
+char cifra_piu_significativa (int n){
+	return to_string(n)[0];
 }
 //funzione
 bool verifica_cifra(int n1,int n2)		{
 	int prodotto = n1 * n2;
 	int somma = n1 + n2;
 	char cifra_prodotto =
-cifra_più_significativa(prodotto);
+cifra_piu_significativa(prodotto);
 	char cifra_somma =
-cifra_più_significativa(somma);
+cifra_piu_significativa(somma);
 	return cifra_prodotto < cifra_somma;
 					}
 int main (){
 	int n1, n2;
 	cout << "inserisci due numeri:";
-	cin >> n1 >> n2
+	cin >> n1 >> n2 ;
 		if (verifica_cifra(n1,n2)){
 			cout << "La cifra più significativa del prodotto è minore della somma" << endl;
 		}
